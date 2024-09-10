51978ca978c1439ec3fdb5839bc6891aba84e13c
Cleonildo Jr <cleu.junior@gmail.com>
2024-09-10 14:13:56 -0300

    Easy: 1 Two Sum

---
+++ leetcodedaily/src/main/java/br/com/leetcode/daily/easy/TwoSum.java
@@ -0,0 +1,26 @@
+package br.com.leetcode.daily.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> sum = new HashMap<>();
+        int[] arr = new int[2];
+        int index = 0;
+
+        for (int num : nums) {
+            if (sum.get(num) != null) {
+                arr[0] = sum.get(num);
+                arr[1] = index;
+                break;
+            }
+
+            sum.put(target-num, index);
+            index++;
+        }
+
+        return arr;
+    }
+}

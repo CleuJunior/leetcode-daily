b5f4966118461d7185b1733b1fdc413fe4bb8fbb
Cleonildo Jr <cleu.junior@gmail.com>
2024-09-15 16:59:21 -0300

    Medium: 74. Search a 2D Matrix

---
+++ leetcodedaily/src/test/java/br/com/leetcode/daily/medium/TwoDMatrixTest.java
@@ -0,0 +1,16 @@
+package br.com.leetcode.daily.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TwoDMatrixTest {
+
+    @Test
+    void basicTests() {
+        assertTrue(TwoDMatrix.searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3));
+        assertFalse(TwoDMatrix.searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 13));
+    }
+
+}

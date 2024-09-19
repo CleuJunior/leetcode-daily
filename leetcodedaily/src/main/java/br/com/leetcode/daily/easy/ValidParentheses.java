891353f7a4c3a77d0a3383318a46558120741d77
Cleonildo Jr <cleu.junior@gmail.com>
2024-09-18 23:32:05 -0300

    Easy: 20. Valid Parentheses

---
+++ leetcodedaily/src/main/java/br/com/leetcode/daily/easy/ValidParentheses.java
@@ -0,0 +1,26 @@
+package br.com.leetcode.daily.easy;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> closeToOpen = Map.of(')', '(', ']', '[', '}', '{');
+
+        for (char c : s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+                if (!closeToOpen.isEmpty() && closeToOpen.get(c).equals(stack.peek())) {
+                    stack.pop();
+                } else
+                    return false;
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
